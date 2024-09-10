d44c7dcbe4c1cbf12ef4a03e3377ef505f351c03
dylan-bruner-student <dybr78f@student.oaisd.org>
2024-09-10 16:16:17 +0000

    CODE!!!

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("Enter a number: ");
+
+string number = Console.ReadLine();
+
+string rep = "";
+
+switch (number) {
+    case "0":
+        rep = "Zero";
+        break;
+    case "1":
+        rep = "One";
+        break;
+    case "2":
+        rep = "Two";
+        break;
+    case "3":
+        rep = "Three";
+        break;
+    case "4":
+        rep = "Four";
+        break;
+    case "5":
+        rep = "Five";
+        break;
+    case "6":
+        rep = "Six";
+        break;
+    case "7":
+        rep = "Seven";
+        break;
+    case "8":
+        rep = "Eight";
+        break;
+    case "9":
+        rep = "Nine";
+        break;
+    case "10":
+        rep = "Ten";
+        break;
+    default:
+        Console.WriteLine("That number is not in range.");
+        break;
+}
+
+Console.WriteLine($"Your number is {rep}");
